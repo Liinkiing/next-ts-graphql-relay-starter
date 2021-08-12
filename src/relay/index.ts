dba1149a939e181d7fd97307eb67d5085d46fe63
Omar Jbara <omar.jbara2@gmail.com>
2021-08-12 02:31:55 +0200

    - Fixed prettier

--- src/relay/index.ts
+++ src/relay/index.ts
@@ -2,9 +2,8 @@ import RelaySSR from 'react-relay-network-modern-ssr/node8/server'
 import { Environment } from 'relay-runtime'
 import { RecordMap } from 'relay-runtime/lib/store/RelayStoreTypes'
 
-export const { initEnvironment, createEnvironment } = (typeof window === 'undefined'
-  ? require('./server')
-  : require('./client')
+export const { initEnvironment, createEnvironment } = (
+  typeof window === 'undefined' ? require('./server') : require('./client')
 ).default as {
   initEnvironment: () => { environment: Environment; relaySSR: RelaySSR }
   createEnvironment: (relayRecords?: RecordMap) => Environment
